4ad4c3aac38c9ee64be4de56a90e58b5a751e90d
Silva (DEV-Team) <Silva@ptytechnologies.com>
2020-11-09 14:59:30 -0500

    A bit of tweaking just to help a bro

--- calculator.js
+++ calculator.js
@@ -1,41 +1,110 @@
-window.onload = function () {
-    resultado = document.getElementById("resultado");
-    
-}
-x = "0";
-xi = 1;
-coma = 0;
-opi = "no"; //operacion en curso
-ni = 0; //numero en espera
+/**
+ * Allows cleaner code when using web JavaScript
+ */
+"use strict";
 
-function number(xx) {
-    if (x == "0" || xi == 1) {
-        resultado.innerHTML = xx;
-        x = xx;
+(function() {
+
+  /**
+   * Pull every single element from DOM you might need
+   */
+  var numberButtons = document.getElementsByClassName('number-button');
+  var operationButtons = document.getElementsByClassName('operation-button');
+  var equalButton = document.getElementsByClassName('equal-button')[0];
+  var clearButton = document.getElementById('clear-button');
+  var displaySpan = document.getElementById('resultado');
+
+  /**
+   * Initialize calculations
+   */
+  var firstDigit;
+  var secondDigit;
+  var operationTypes;
+  var currentOperation;
+  var isFirstDigit;
+  var result;
+
+  /**
+   * Assign later for cleaner code
+   */
+  firstDigit = '';
+  secondDigit = '';
+  isFirstDigit = true;
+  operationTypes = ['+', '-', '*', '/'];
+  currentOperation = 0;
+  result = '';
+
+  /**
+   * This works just as assigning "onclick", but its cleaner as we keep functionality only to javascript
+   */
+  var init = () => {
+    for (var i = 0; i < numberButtons.length; i++) numberButtons[i].addEventListener('click', handleNumberButtonClick, false);
+    for (var i = 0; i < operationButtons.length; i++) operationButtons[i].addEventListener('click', handleOperationButtonClick, false);
+    equalButton.addEventListener('click', handleEqualButtonClick, false);
+    clearButton.addEventListener('click', handleClearButtonClick, false);
+  }
+
+  /**
+   * Handles the number click to create the first or second digit
+   * @param {HTML Element} e 
+   */
+  var handleNumberButtonClick = e => {
+    if (isFirstDigit) {
+      firstDigit += e.target.value;
+      displaySpan.innerHTML = firstDigit;
     } else {
-        resultado.innerHTML += xx;
-        x += xx;
+      secondDigit += e.target.value;
+      displaySpan.innerHTML = secondDigit;
     }
-    xi = 0;
-}
+  }
 
-function operation(s) {
-    igualdad()
-    ni = x
-    opi = s;
-    xi = 1;
-}
+  /**
+   * Handles the operation to be done later on equality press
+   * @param {HTML Element} e 
+   */
+  var handleOperationButtonClick = e => {
+    currentOperation = operationTypes.indexOf(e.target.value);
+    isFirstDigit = false;
+  }
 
-function igualdad() {
-    if (opi == "no") {
-        resultado.innerHTML = x;
-    } else {
-        cadena = ni + opi + x;
-        solucion = eval(cadena);
-        resultado.innerHTML = solucion;
-        x = solucion;
-        opi = "no";
-        xi = 1;
-        console.log(solucion);
+  /**
+   * Handles clear button so user can start over again
+   * @param {HTML Element} e 
+   */
+  var handleClearButtonClick = e => {
+    firstDigit = '';
+    secondDigit = '';
+    currentOperation = 0;
+    result = '';
+    displaySpan.innerHTML = 0
+    isFirstDigit = true;
+  }
+
+  /**
+   * Resolves the equation
+   * @param {HTML Element} e 
+   */
+  var handleEqualButtonClick = e => {
+    switch (currentOperation) {
+      case 0:
+        result = parseInt(firstDigit) + parseInt(secondDigit);
+        break;
+      case 1:
+        result = parseInt(firstDigit) - parseInt(secondDigit);
+        break;
+      case 2:
+        result = parseInt(firstDigit) * parseInt(secondDigit);
+        break;
+      case 3:
+        result = parseInt(firstDigit) / parseInt(secondDigit);
+        break;
     }
-}
+    displaySpan.innerHTML = result;
+  }
+
+  /**
+   * Initialize at the end
+   */
+  init();
+
+})();
